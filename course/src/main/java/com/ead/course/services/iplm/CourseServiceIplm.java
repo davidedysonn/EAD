13f03e7df99269518af13874704b8a6cac24854e
David Edysonn Belfort Neves <34004075+davidedysonn@users.noreply.github.com>
2025-07-31 23:08:22 -0300

    Feat: CRUD completo de Course, com validacao com os respectivos metodos Http, adicao de dependencia validadora no pom.

--- course/src/main/java/com/ead/course/services/iplm/CourseServiceIplm.java
+++ course/src/main/java/com/ead/course/services/iplm/CourseServiceIplm.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CourseServiceIplm implements CourseService {
@@ -38,5 +40,20 @@ public class CourseServiceIplm implements CourseService {
             moduleRepository.deleteAll(moduleModelList);
         }
         courserepository.delete(courseModel);
+    }
+
+    @Override
+    public CourseModel save(CourseModel courseModel) {
+        return courserepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return courserepository.findById(courseId);
+    }
+
+    @Override
+    public List<CourseModel> findAll() {
+        return courserepository.findAll();
     }
 }
